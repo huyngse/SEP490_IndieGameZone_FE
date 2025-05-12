d2d350be5a61cd6642db91e13aed5b5a4f191484
huyngse <ngogiahuy775309@gmail.com>
2025-05-12 14:27:37 +0700

    chore(set up): set up dark theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,33 @@
-import { Button } from "antd";
+import { Button, Steps } from "antd";
+import MaxWidthWrapper from "./components/max-width-wrapper";
+const description = 'This is a description.';
 
 function App() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <Button type="primary">Click me!</Button>
-    </div>
+    <MaxWidthWrapper>
+      <div className="text-center p-5">
+        <h1 className="text-5xl font-bold">IndieGameZone</h1>
+      </div>
+      <Button type="primary" className="mb-5">Click me!</Button>
+      <Steps
+        current={1}
+        items={[
+          {
+            title: "Finished",
+            description,
+          },
+          {
+            title: "In Progress",
+            description,
+            subTitle: "Left 00:00:08",
+          },
+          {
+            title: "Waiting",
+            description,
+          },
+        ]}
+      />
+    </MaxWidthWrapper>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { ConfigProvider } from "antd";
+import { ConfigProvider, theme } from "antd";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ConfigProvider
       theme={{
+        algorithm: theme.darkAlgorithm,
         token: {
           colorPrimary: "#FF6600",
           borderRadius: 2,
