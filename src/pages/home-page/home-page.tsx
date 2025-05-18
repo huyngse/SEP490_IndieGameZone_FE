f048cb63af545ab6e15b9605004e16c0c1d09ac1
huyngse <ngogiahuy775309@gmail.com>
2025-05-18 18:45:02 +0700

    feat(home page): search section

--- src/pages/home-page/home-page.tsx
+++ src/pages/home-page/home-page.tsx
@@ -2,6 +2,9 @@ import MaxWidthWrapper from "@/components/max-width-wrapper";
 import { Link } from "react-router-dom";
 import FeaturedGameSection from "./featured-game-section";
 import SpecialOffersSection from "./special-offers-section";
+import { IoTimer } from "react-icons/io5";
+import { MdAttachMoney, MdMoneyOff } from "react-icons/md";
+import { FaTags } from "react-icons/fa";
 
 const HomePage = () => {
   return (
@@ -19,6 +22,39 @@ const HomePage = () => {
       </div>
       <FeaturedGameSection />
       <SpecialOffersSection />
+      <section className="pb-9">
+        <h2 className="text-2xl font-bold text-center my-5">Tìm Kiếm Game</h2>
+        <div className="grid grid-cols-4 gap-3">
+          <Link
+            to={"/search/latest"}
+            className="cursor-box btn-1 font-bold drop-shadow relative"
+          >
+            <IoTimer className="absolute size-24 -right-7 text-zinc-500 -z-10" />
+            Mới Phát Hành
+          </Link>
+          <Link
+            to={"/search/sales"}
+            className="cursor-box btn-1 font-bold drop-shadow relative"
+          >
+            <MdAttachMoney className="absolute size-24 -right-7 text-zinc-500 -z-10" />
+            Ưu Đãi
+          </Link>
+          <Link
+            to={"/search?maxPrice=0"}
+            className="cursor-box btn-1 font-bold drop-shadow relative"
+          >
+            <MdMoneyOff className="absolute size-24 -right-7 text-zinc-500 -z-10" />
+            Miễn Phí
+          </Link>
+          <Link
+            to={"/tags"}
+            className="cursor-box btn-1 font-bold drop-shadow relative"
+          >
+            <FaTags className="absolute size-24 -right-7 text-zinc-500 -z-10" />
+            Bằng Tags
+          </Link>
+        </div>
+      </section>
     </MaxWidthWrapper>
   );
 };
