7aa6a33a56dcd278b9d357330a08cc94d64d3ce8
huyngse <ngogiahuy775309@gmail.com>
2025-06-22 08:06:52 +0700

    fix: remove manualChunks

--- vite.config.ts
+++ vite.config.ts
@@ -12,17 +12,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  build: {
-    chunkSizeWarningLimit: 600,
-    minify: 'terser',
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return id.toString().split('node_modules/')[1].split('/')[0].toString();
-          }
-        }
-      }
-    }
-  }
 })
