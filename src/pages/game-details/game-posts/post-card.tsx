e3cd89d1b3d3c0899841bfed32449c57a3950c7b
huyngse <ngogiahuy775309@gmail.com>
2025-07-16 21:04:22 +0700

    style(view post): game posts interface

--- src/pages/game-details/game-posts/post-card.tsx
+++ src/pages/game-details/game-posts/post-card.tsx
@@ -1,84 +1,131 @@
-import { Avatar, Image, Tag } from "antd";
-import { CiClock1 } from "react-icons/ci";
-import { FaRegComment, FaRegHeart } from "react-icons/fa";
+import { Avatar, Button, Dropdown, MenuProps, Tag } from "antd";
+import { useState } from "react";
+import {
+  FaChevronLeft,
+  FaChevronRight,
+  FaFlag,
+  FaLink,
+  FaRegComment,
+  FaRegHeart,
+} from "react-icons/fa";
+import { IoMdMore } from "react-icons/io";
 import { IoShareSocialOutline } from "react-icons/io5";
+import Lightbox from "yet-another-react-lightbox";
 
 const PostCard = () => {
+  const IMAGES = [
+    "https://placehold.co/600x400?text=Image+1",
+    "https://placehold.co/600x400?text=Image+2",
+    "https://placehold.co/600x400?text=Image+3",
+  ];
+
+  const [index, setIndex] = useState(-1); // for lightbox
+  const [currentImage, setCurrentImage] = useState(0); // for slider
+
+  const slides = IMAGES.map((image) => ({
+    src: image,
+  }));
+
+  const moreOptionItems: MenuProps["items"] = [
+    {
+      label: <div>Copy link to post</div>,
+      icon: <FaLink />,
+      key: "0",
+    },
+    {
+      label: <div>Report post</div>,
+      key: "1",
+      icon: <FaFlag />,
+    },
+  ];
+
+  const handlePrev = () => {
+    setCurrentImage((prev) => (prev === 0 ? IMAGES.length - 1 : prev - 1));
+  };
+
+  const handleNext = () => {
+    setCurrentImage((prev) => (prev === IMAGES.length - 1 ? 0 : prev + 1));
+  };
+
   return (
     <div>
-      <div className="bg-zinc-800 w-full p-5">
+      <Lightbox
+        index={currentImage}
+        slides={slides}
+        open={index >= 0}
+        close={() => setIndex(-1)}
+      />
+      <div className="bg-zinc-800 w-full p-3">
         <div className="flex justify-between items-center gap-3">
           <div className="flex items-center gap-3">
             <Avatar src="https://cdn.lazi.vn/storage/uploads/users/avatar/1693142947_lazi_435254.jpg" />
-            <span className="font-semibold text-lg ">Lorem ipsum</span>
-          </div>
-          <div className="flex items-center gap-2 ">
-            <CiClock1 />
-            <span className="text-xs text-gray-400">2 days ago</span>
-          </div>
-        </div>
-        <div className="mt-5">
-          <div className="flex flex-col gap-3">
-            {" "}
-            <span className="font-bold text-xl">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid sed id veniam rerum possimus voluptas,
-              modi qui dolor aspernatur.
-            </span>
-            <p className=" text-gray-400">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas assumenda voluptatem fuga veritatis
-              recusandae debitis, totam facere omnis ipsam enim, a doloribus soluta similique pariatur. Minus sit
-              repudiandae id. Minima! A, voluptatum sequi iste facilis quisquam aliquam minima sed, aut reiciendis nulla
-              omnis vero non molestiae at nam consequatur totam consequuntur quaerat deserunt distinctio. Ipsam
-              provident expedita esse facilis nostrum? Modi debitis adipisci nostrum fugit dolor enim quia perspiciatis
-              deleniti, voluptas laboriosam cum, mollitia libero repellat veritatis ullam. Doloremque asperiores atque
-              eius impedit alias vero possimus quasi soluta, consectetur eveniet?
-            </p>
-          </div>
-          <div className="flex flex-col gap-4">
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mt-5">
-              <Image
-                className="w-full h-[300px] object-cover rounded-lg mt-5"
-                src="https://steamcdn-a.akamaihd.net/steam/apps/921800/header.jpg"
-                alt="Post Image"
-              />
-              <Image
-                className="w-full h-[300px] object-cover rounded-lg mt-5"
-                src="https://steamcdn-a.akamaihd.net/steam/apps/921800/header.jpg"
-                alt="Post Image"
-              />
-              <Image
-                className="w-full h-[300px] object-cover rounded-lg mt-5"
-                src="https://steamcdn-a.akamaihd.net/steam/apps/921800/header.jpg"
-                alt="Post Image"
-              />
-            </div>
             <div>
-              <Tag color="cyan" style={{ borderRadius: "9999px", padding: "2px 12px", fontSize: "13px" }}>
-                #Announcement
-              </Tag>
-              <Tag color="cyan" style={{ borderRadius: "9999px", padding: "2px 12px", fontSize: "13px" }}>
-                #Bug
-              </Tag>
-              <Tag color="cyan" style={{ borderRadius: "9999px", padding: "2px 12px", fontSize: "13px" }}>
-                #Solution
-              </Tag>
+              <div className="font-semibold">Lorem ipsum</div>
+              <div className="text-xs text-gray-400">2 days ago</div>
             </div>
           </div>
-          <div>
-            <div className="flex  items-center gap-4 mt-8">
-              <div className="flex items-center gap-2 text-gray-400 ">
-                <FaRegHeart  size={23} />
-                <span>1000</span>
-              </div>
+          <Dropdown menu={{ items: moreOptionItems }}>
+            <Button icon={<IoMdMore />} shape="circle" type="text"></Button>
+          </Dropdown>
+        </div>
 
-              <div className="flex items-center gap-2 text-gray-400 ">
-                <FaRegComment size={20} />
-                <span>1000</span>
-              </div>
-              <div className="text-gray-400">
-                <IoShareSocialOutline size={23} />
-              </div>
-            </div>
+        <div className="mt-2">
+          <div className="flex flex-col gap-3">
+            <span className="font-bold text-xl">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid
+              sed id veniam rerum possimus voluptas, modi qui dolor aspernatur.
+            </span>
+          </div>
+
+          <div className="flex flex-wrap mt-2">
+            <Tag color="orange">#Announcement</Tag>
+            <Tag color="orange">#Bug</Tag>
+            <Tag color="orange">#Solution</Tag>
+          </div>
+
+          <div className="relative">
+            <button
+              onClick={handlePrev}
+              className="absolute left-2 bottom-1/2 translate-y-1/2 p-3 rounded-full bg-zinc-500/40 cursor-pointer hover:bg-zinc-500/60 duration-300 z-10"
+            >
+              <FaChevronLeft />
+            </button>
+            <img
+              className="w-full object-contain aspect-video rounded bg-zinc-900 my-2 cursor-pointer"
+              src={IMAGES[currentImage]}
+              alt={`Post Image ${currentImage + 1}`}
+              onClick={() => setIndex(currentImage)}
+            />
+            <button
+              onClick={handleNext}
+              className="absolute right-2 bottom-1/2 translate-y-1/2 p-3 rounded-full bg-zinc-500/40 cursor-pointer hover:bg-zinc-500/60 duration-300 z-10"
+            >
+              <FaChevronRight />
+            </button>
+          </div>
+
+          <div className="flex items-center gap-3 mt-2">
+            <Button
+              icon={<FaRegHeart className="text-gray-400" />}
+              shape="round"
+              type="text"
+            >
+              <span>1000</span>
+            </Button>
+
+            <Button
+              icon={<FaRegComment className="text-gray-400" />}
+              shape="round"
+              type="text"
+            >
+              <span>1000</span>
+            </Button>
+
+            <Button
+              icon={<IoShareSocialOutline className="text-gray-400" />}
+              shape="circle"
+              type="text"
+            />
           </div>
         </div>
       </div>
