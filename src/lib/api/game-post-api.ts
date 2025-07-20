8fa35ff44179c0ccd76d50ad4fd5910cb19f4ac8
huyngse <ngogiahuy775309@gmail.com>
2025-07-20 18:40:49 +0700

    feat(game forum): infinite scroll

--- src/lib/api/game-post-api.ts
+++ src/lib/api/game-post-api.ts
@@ -1,4 +1,4 @@
-import { toFormData } from "../object";
+import { toFormData, toSearchParams } from "../object";
 import { axiosClient } from "./config/axios-client";
 
 export const handleApiError = (error: any): { error: string | null; data: any; success: boolean } => {
@@ -10,6 +10,7 @@ export const handleApiError = (error: any): { error: string | null; data: any; s
     return { error: "An unexpected error occurred.", data: null, success: false };
   }
 };
+
 export interface PostData {
   Title: string;
   Content: string;
@@ -26,9 +27,11 @@ export const createPost = async (userId: string, gameId: string, postData: PostD
     return handleApiError(error);
   }
 };
+
 export interface PostCommentData {
   Content: string;
 }
+
 export const createPostComment = async (postId: string, userId: string, postCommentData: PostCommentData) => {
   try {
     const formData = toFormData(postCommentData);
@@ -38,6 +41,7 @@ export const createPostComment = async (postId: string, userId: string, postComm
     return handleApiError(error);
   }
 };
+
 export const createReactionPost = async (userId: string, postId: string) => {
   try {
     const { data } = await axiosClient.post(`/api/users/${userId}/posts/${postId}/post-reactions`);
@@ -46,17 +50,27 @@ export const createReactionPost = async (userId: string, postId: string) => {
     return handleApiError(error);
   }
 };
-export const getGamePosts = async (gameId: string) => {
+
+type GetGamePostParams = {
+  PageNumber?: number,
+  PageSize?: number
+}
+
+export const getGamePosts = async (gameId: string, params: GetGamePostParams) => {
+  console.log(params);
   try {
-    const { data } = await axiosClient.get(`/api/games/${gameId}/posts`);
-    return { error: null, data: data, success: true };
+    const { data, headers } = await axiosClient.get(`/api/games/${gameId}/posts${toSearchParams(params)}`);
+    return {
+      error: null, data: { posts: data, headers }, success: true
+    };
   } catch (error) {
     return handleApiError(error);
   }
 };
-export const getReactionPost= async (userId: string, postId: string) => {
+
+export const getReactionPost = async (userId: string, postId: string) => {
   try {
-    const { data } = await axiosClient.get(`/api/users/${userId}/posts/${postId}/post-reactions`);
+    const { data } = await axiosClient.get(`/ api / users / ${userId} / posts / ${postId} / post - reactions`);
     return { error: null, data: data, success: true };
   } catch (error) {
     return handleApiError(error);
--- src/lib/object.ts
+++ src/lib/object.ts
@@ -38,6 +38,17 @@ export function toFormData(obj: Record<string, any>): FormData {
   return formData;
 }
 
+export const toSearchParams = (params: Record<string, string | number | boolean | undefined | null>): string => {
+  const query = Object.entries(params)
+    .filter(([_, value]) => value !== undefined && value !== null)
+    .map(
+      ([key, value]) => `${encodeURIComponent(key)}=${encodeURIComponent(String(value))}`
+    )
+    .join('&');
+
+  return query ? `?${query}` : '';
+};
+
 export function areArraysEqual(arr1: string[], arr2: string[]): boolean {
   if (arr1.length !== arr2.length) return false;
   return arr1.every((val, index) => val === arr2[index]);
