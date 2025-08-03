20cd470cd82fb18ddf8d22d56b38f52a3a61188b
vuvuvo0303 <levanhoangvu2003@gmail.com>
2025-08-04 00:25:30 +0700

    feat(commercial registration by userId): done fetch

---
+++ src/pages/developer/dev-manage-commericial-package/manage-commercial-package-registrations/columns.tsx
@@ -0,0 +1,80 @@
+// columns.tsx
+import { CommercialPackage } from "@/types/commercial-package";
+import { Tag } from "antd";
+import { ColumnsType } from "antd/es/table";
+import ActionMenu from "./action-menu";
+
+export const getCommercialRegistrationColumns = (onRefresh?: () => void): ColumnsType<CommercialPackage> => [
+  {
+    title: "Developer Name",
+    dataIndex: "developer",
+    key: "developer.userName",
+    render: (developer: { userName: string }) => developer.userName || "N/A",
+  },
+  {
+    title: "Game Name",
+    dataIndex: "gameName",
+    key: "gameName",
+    render: (name: string) => name || "N/A",
+  },
+  {
+    title: "Commercial Package Name",
+    dataIndex: "commercialPackageName",
+    key: "commercialPackageName",
+    render: (name: string) => name || "N/A",
+  },
+  
+  {
+    title: "Duration (days)",
+    dataIndex: "duration",
+    key: "duration",
+  },
+  {
+    title: "Price ",
+    dataIndex: "price",
+    key: "price",
+    render: (price: number) => `${price?.toLocaleString()} VND`,
+  },
+
+  {
+  title: "Status",
+  dataIndex: "status",
+  key: "status",
+  render: (status: string) => {
+    let color = "default";
+    switch (status.toLowerCase()) {
+      case "pending":
+        color = "blue";
+        break;
+      case "active":
+        color = "green";
+        break;
+      case "expired":
+        color = "red";
+        break;
+      case "cancelled":
+        color = "orange";
+        break;
+      case "failed":
+        color = "volcano";
+        break;
+    }
+    return <Tag color={color}>{status}</Tag>;
+  },
+},
+  {
+    title: "Start Date",
+    dataIndex: "startDate",
+    key: "startDate",
+  },
+  {
+    title: "End Date",
+    dataIndex: "endDate",
+    key: "endDate",
+  },
+    {
+      title: "Action",
+      key: "action",
+      render: (_, record: CommercialPackage) => <ActionMenu record={record} onSuccess={onRefresh} />,
+    },
+];
--- src/pages/developer/dev-manage-commericial-package/manage-commercial-package-registrations/dev-commercial-package-registrations.tsx
+++ src/pages/developer/dev-manage-commericial-package/manage-commercial-package-registrations/dev-commercial-package-registrations.tsx
@@ -1,5 +1,59 @@
-const DevCommercialPackageRegistrations = () => {
-  return <div>Manage Commercial Package Registrations</div>;
+import React, { useCallback, useEffect, useState } from "react";
+import { Table, Typography, message } from "antd";
+import {
+  getCommercialPackagesRegistrationsByUserId,
+} from "@/lib/api/commercial-package-api";
+import { CommercialPackage } from "@/types/commercial-package";
+import useAuthStore from "@/store/use-auth-store";
+import { getCommercialRegistrationColumns } from "./columns";
+
+const { Title } = Typography;
+
+const DevCommercialPackageRegistrations: React.FC = () => {
+  const [commercialPackages, setCommercialPackages] = useState<CommercialPackage[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [messageApi, contextHolder] = message.useMessage();
+  const { profile } = useAuthStore();
+
+  const fetchCommercialPackagesByUserId = useCallback(async () => {
+    setLoading(true);
+    try {
+      const response = await getCommercialPackagesRegistrationsByUserId(profile?.id || "");
+      if (response.error) {
+        throw new Error(response.error);
+      }
+      setCommercialPackages(response.data);
+    } catch (error) {
+      console.error("Failed to fetch reports:", error);
+      messageApi.error("Failed to fetch reports");
+    } finally {
+      setLoading(false);
+    }
+  }, [messageApi, profile?.id]);
+
+  useEffect(() => {
+    if (profile?.id) {
+      fetchCommercialPackagesByUserId();
+    }
+  }, [fetchCommercialPackagesByUserId, profile?.id]);
+
+  return (
+    <>
+      {contextHolder}
+      <div className="flex justify-center mb-6">
+        <Title level={2}>Manage Commercial Packages Registrations</Title>
+      </div>
+      <div>
+        <Table
+          dataSource={commercialPackages}
+          columns={getCommercialRegistrationColumns(fetchCommercialPackagesByUserId)}
+          rowKey="id"
+          loading={loading}
+          pagination={{ pageSize: 5 }}
+        />
+      </div>
+    </>
+  );
 };
 
 export default DevCommercialPackageRegistrations;
