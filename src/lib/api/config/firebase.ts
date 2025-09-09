52f12cb95e7d28f8f5d1574875d4a19b9696c2df
huyngse <ngogiahuy775309@gmail.com>
2025-09-09 17:30:43 +0700

    chore: remove secret keys

--- src/lib/api/config/firebase.ts
+++ src/lib/api/config/firebase.ts
@@ -3,12 +3,12 @@ import { getStorage } from "firebase/storage";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
- apiKey: "***REMOVED***",
-  authDomain: "***REMOVED***",
-  projectId: "***REMOVED***",
-  storageBucket: "***REMOVED***.firebasestorage.app",
-  messagingSenderId: "***REMOVED***",
-  appId: "1:***REMOVED***:web:09e6d9b10cd5f6e1ad531a"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 
@@ -18,7 +18,7 @@ const googleProvider = new GoogleAuthProvider();
 const auth = getAuth();
 
 
-export { storage,googleProvider,auth };
+export { storage, googleProvider, auth };
 
 
 
